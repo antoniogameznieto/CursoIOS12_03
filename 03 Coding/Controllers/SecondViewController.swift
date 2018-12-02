03a5ebc53978fe17ea9fb4ad98aae414408ff113
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-02 16:20:39 +0100

    Terminado el capitulo 46

--- 03 Coding/Controllers/SecondViewController.swift
+++ 03 Coding/Controllers/SecondViewController.swift
@@ -14,30 +14,48 @@ class SecondViewController: UIViewController {
     var fibonacci : [Int] = [0,1]   // Array que contenga la serie de Fibonacci
     var fibId = 1                   // Máximo de elementos de la serie que quiera obtener
     
+    // Definimos los IBOutlets
+    @IBOutlet weak var etiquetaNumero: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
     // Método de carga del SecondViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Inicializamos el máximo de números a solicitar
-        fibId = 80;
-        // Los generamos
-        generateFibNumbers()
+        // Al arrancar el viewController debemos actualizar la etiqueta y generar los números aleatorios
+        actualizarEtiqueta(id: Int(self.stepper.value))
     }
 
     // Función propia que genera los números Fibonacci y los mete en el array hasta el máximo definido en la variable anterior
     func generateFibNumbers() {
-        if(fibId <= 1 || fibId > 100){ // Si el maximo de números no está entre 1 y 100, entonces no hacemos nada
-            return
-        }
         
-        fibonacci = [0,1]
+        
         // Aqui generaremos los números hasta el identificador
+        /*if fibId < 2 {
+            return
+        }*/
+        fibonacci = [0,1]
         for i in 2...fibId { // desde el 2 hasta el fibId (Incluyendo los dos)
             // Calculamos el número nuevo y lo añadimos al array (colección)
             fibonacci.append(fibonacci[i-1]+fibonacci[i-2])
             // Lo mostramos por la consola
-            print(fibonacci[i])
+            print(fibonacci)
         }
     }
 
+    func actualizarEtiqueta(id: Int){
+        fibId = id
+        self.etiquetaNumero.text = "\(self.fibId)"
+        generateFibNumbers()
+    }
+    
+    // Métodos y acciones
+    // Acción cuando cambia el valor del stepper
+    @IBAction func stepperPulsado(_ sender: UIStepper) {
+        actualizarEtiqueta(id: Int(sender.value))
+    }
+    
+    
+    
 }
 
