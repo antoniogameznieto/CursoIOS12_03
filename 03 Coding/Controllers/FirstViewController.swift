15853c1655bb5e2e05e6a3546fc0c46b505d2b53
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-02 12:09:18 +0100

    Acabado el slider y sus métodos

--- 03 Coding/Controllers/FirstViewController.swift
+++ 03 Coding/Controllers/FirstViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+// Hemos añadido al firstViewControler el permiso para que los textboxes
+// deleguen funciones al controlador
+class FirstViewController: UIViewController, UITextFieldDelegate {
+    
+    // IB Outlets
+    @IBOutlet weak var labelEdad: UILabel! // Etiqueta de Edad
+    @IBOutlet weak var sliderEdad: UISlider! // Slider de Edad
+    
+    // Definimos variables globales
+    var edadUsuario : Int = -1 // Edad del usuario
+    
+    // Método de carga del firstViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Llamamos a la función que actualiza la edad en la etiqueta
+        self.actualizarEtiquetaEdad()
+        
     }
-
-
+    
+    // ¿Que pasa cuando pulsamos un enter en el textField
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Cierra el teclado
+        textField.resignFirstResponder()
+        // Recuperamos el contenido del texfield si es que existe
+        if textField.text != nil { // Si el contenido del textField está lleno
+            // Imprimimos con consola el texto del textfield
+            print(textField.text as Any)
+        }
+        // Finalizamos la edición del textField
+        return true
+    }
+    
+    // Metodo cambio valor del slider
+    @IBAction func sliderEdadMoved(_ sender: UISlider) {
+        // Llamamos a la función que actualiza la edad en la etiqueta
+        self.actualizarEtiquetaEdad()
+    }
+    
+    func actualizarEtiquetaEdad(){
+        // Leemos el valor del slider y lo asignamos
+        edadUsuario = Int(self.sliderEdad.value)
+        //Cambiamos el valor de la etiqueta de la edad
+        self.labelEdad.text = "\(edadUsuario)"
+    }
+    
 }
 
