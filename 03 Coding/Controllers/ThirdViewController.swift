629546ac64d10769c6104d2f86c8ab79e7a591ff
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-02 09:07:34 +0100

    Añadimos un View Controller Nuevo

---
+++ 03 Coding/Controllers/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  03 Coding
+//
+//  Created by antoniogamez on 2/12/18.
+//  Copyright © 2018 Adhoc. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
