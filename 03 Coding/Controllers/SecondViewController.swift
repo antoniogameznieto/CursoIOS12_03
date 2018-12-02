9a933a64ee5f28ed68a419c6218065046a9367f3
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-02 16:46:57 +0100

    Terminada la clase 47. Fibonnaci listo

--- 03 Coding/Controllers/SecondViewController.swift
+++ 03 Coding/Controllers/SecondViewController.swift
@@ -12,12 +12,12 @@ class SecondViewController: UIViewController {
 
     // Definimos las variables
     var fibonacci : [Int] = [0,1]   // Array que contenga la serie de Fibonacci
-    var fibId = 1                   // Máximo de elementos de la serie que quiera obtener
+    var fibId = 1                    // Máximo de elementos de la serie que quiera obtener
     
     // Definimos los IBOutlets
     @IBOutlet weak var etiquetaNumero: UILabel!
     @IBOutlet weak var stepper: UIStepper!
-    
+    @IBOutlet weak var textViewResult: UITextView!
     
     // Método de carga del SecondViewController
     override func viewDidLoad() {
@@ -28,30 +28,34 @@ class SecondViewController: UIViewController {
 
     // Función propia que genera los números Fibonacci y los mete en el array hasta el máximo definido en la variable anterior
     func generateFibNumbers() {
-        
-        
         // Aqui generaremos los números hasta el identificador
-        /*if fibId < 2 {
-            return
-        }*/
         fibonacci = [0,1]
+        //fibonacci = [0]
         for i in 2...fibId { // desde el 2 hasta el fibId (Incluyendo los dos)
             // Calculamos el número nuevo y lo añadimos al array (colección)
             fibonacci.append(fibonacci[i-1]+fibonacci[i-2])
-            // Lo mostramos por la consola
-            print(fibonacci)
         }
+        // Pasamos el array fibonacci a String, de una sóla vez
+        let fibStr : [String] = fibonacci.compactMap({String($0)})
+        // A la variable result le mandamos el array separadito ya en String
+        let result : String = fibStr.joined(separator: "\n")
+        // Actualizamos la etiqueta donde vamos a mostrar el resultado
+        self.textViewResult.text = result
     }
 
     func actualizarEtiqueta(id: Int){
+        // Asignamos el id que metemos en una variable
         fibId = id
+        // Actualizamos la etiqueta
         self.etiquetaNumero.text = "\(self.fibId)"
+        // Llamamos a la generación de números
         generateFibNumbers()
     }
     
     // Métodos y acciones
     // Acción cuando cambia el valor del stepper
     @IBAction func stepperPulsado(_ sender: UIStepper) {
+        // Actualizamos las etiquetas
         actualizarEtiqueta(id: Int(sender.value))
     }
     
